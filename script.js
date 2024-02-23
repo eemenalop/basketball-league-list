542a13d61b1c2dc915c9d1fd5a9b8662dc55ad02
eemenalop <eemena26@gmail.com>
2024-02-23 12:26:59 -0400

    Add fuction removeSelectedPlayer from list of players

--- script.js
+++ script.js
@@ -5,12 +5,21 @@ let removePlayerBtn = document.getElementById("removePlayerBtn");
 let list = document.getElementById("list");
 
 
+//Event listeners to each list item
+const assignEventListeners = () => {
+    const listItems = document.querySelectorAll('#list li');
+    listItems.forEach(player => {
+        player.addEventListener("click", selectPlayer);
+    });
+}
+
+
 //Add Player to list
 const addPlayer = () => {
 
     let playerName = playerInput.value.trim();
 
-    if (playersList.includes(playerName)){
+    if (playersList.includes(playerName)) {
         alert('Player already exists');
         return;
     }
@@ -18,12 +27,34 @@ const addPlayer = () => {
     if (playerInput.value === '') {
         alert('Please insert a player');
         return;
-    }  
-        playersList.push(playerInput.value.trim());
-        list.innerHTML += `<li>${playerInput.value}</li>`;
-        playerInput.value = "";
-    
+    }
+    playersList.push(playerInput.value.trim());
+    list.innerHTML += `<li>${playerInput.value}</li>`;
+    playerInput.value = "";
 
+    assignEventListeners();
 }
 
+//Highlight the player selected
+const selectPlayer = (event) => {
+    event.target.classList.toggle("player-selected");
+}
+
+//Remove player from list
+const removeSelectedPlayer = () => {
+    const selectedPlayer = document.querySelector('#list .player-selected');
+
+    if (selectedPlayer) {
+        const index = playersList.indexOf(selectedPlayer.textContent);
+        if (index !== -1) {
+            playersList.splice(index, 1);
+        }
+        selectedPlayer.remove();
+    } else {
+        alert('Please select a player to remove');
+    }
+}
+
+removePlayerBtn.addEventListener("click", removeSelectedPlayer);
 addPlayerBtn.addEventListener("click", addPlayer);
+
