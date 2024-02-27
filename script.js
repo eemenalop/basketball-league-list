c6df84a4ff637651830044def04dd16a05b73328
eemenalop <eemena26@gmail.com>
2024-02-27 10:10:02 -0400

    Add Player automatically

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let playerInput = document.getElementById("player");
 let addPlayerBtn = document.getElementById("addPlayerBtn");
 let removePlayerBtn = document.getElementById("removePlayerBtn");
 let list = document.getElementById("list");
+let teamA = document.getElementById("teamA");
+let teamB = document.getElementById("teamB");
+let assignToATeamBtn = document.getElementById('assignToATeamBtn')
 
 
 //Event listeners to each list item
@@ -57,4 +60,42 @@ const removeSelectedPlayer = () => {
 
 removePlayerBtn.addEventListener("click", removeSelectedPlayer);
 addPlayerBtn.addEventListener("click", addPlayer);
+
+//Add player automatically
+const autoAddPlayers = () => {
+    let randomPlayer = `Player ${playersList.length + 1}`;
+    if (Math.random() > 0.7) {
+        playersList.push(randomPlayer);
+        list.innerHTML += `<li>${randomPlayer}</li>`;
+        assignEventListeners();
+    }
+}
+setInterval(autoAddPlayers, 1000);
+
+
+//Assign player to a team
+
+let playersAssignedToTeamA = 0;
+let playersAssignedToTeamB = 0;
+const maxPlayersPerTeam = 5;
+
+const assignSelectedPlayerToTeam = () => {
+
+    const selectedPlayer = document.querySelector('#list .player-selected');
+
+    if (!selectedPlayer) {
+        alert('Please select a player to assign to a team');
+        return;
+    }
+
+    if (playersAssignedToTeamA < maxPlayersPerTeam) {
+        teamA.innerHTML += `<li>${selectedPlayer.textContent}</li>`;
+        playersAssignedToTeamA++;
+    } else if (playersAssignedToTeamB < maxPlayersPerTeam) {
+        teamB.innerHTML += `<li>${selectedPlayer.textContent}</li>`;
+        playersAssignedToTeamB++;
+    } alert('Both teams have reached the maximum number of players')
+}
+
+assignToATeamBtn.addEventListener('click', assignSelectedPlayerToTeam);
 
