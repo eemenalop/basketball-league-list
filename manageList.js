542db4c19a0692215be849f9db196d0d87c6cc64
Edgar Mena <eemena26@gmail.com>
2024-04-01 21:07:02 -0400

    Reordenando archivos

--- manageList.js
+++ manageList.js
@@ -1,2 +1,110 @@
-import * as script from './script.js'
+import {playersList} from "./script.js"
+import { assignEventListeners} from './script.js'
+
+let addPlayerBtn = document.getElementById("addPlayerBtn");
+let playerInput = document.getElementById("player");
+let removePlayerBtn = document.getElementById("removePlayerBtn");
+let teamA = document.getElementById("teamA");
+let arrTeamA = [];
+let teamB = document.getElementById("teamB");
+let arrTeamB = [];
+let assignToTeamABtn = document.getElementById('assignToTeamABtn')
+let assignToTeamBBtn = document.getElementById('assignToTeamBBtn')
+let removePlayerFromTeamBtn = document.getElementById('removePlayerFromTeamBtn')
+
+//Add Player to list
+const addPlayer = () => {
+
+    let playerName = playerInput.value.trim();
+
+    if (playersList.includes(playerName)) {
+        alert('Player already exists');
+        return;
+    }
+
+    if (playerInput.value === '') {
+        alert('Please insert a player');
+        return;
+    }
+    playersList.push(playerInput.value.trim());
+    list.innerHTML += `<li>${playerInput.value}</li>`;
+    playerInput.value = "";
+
+    assignEventListeners('#list li');
+
+}
+addPlayerBtn.addEventListener("click", addPlayer);
+
+//Remove player from list
+const removeSelectedPlayer = (arr, classes) => {
+    const selectedPlayer = document.querySelector(classes);
+
+    if (selectedPlayer) {
+        const index = arr.indexOf(selectedPlayer.textContent);
+        if (index !== -1) {
+            arr.splice(index, 1);
+        }
+        selectedPlayer.remove();
+    } else {
+        alert('Please select a player to remove');
+    }
+    console.log(arr)
+}
+
+removePlayerBtn.addEventListener("click", () => { removeSelectedPlayer(playersList, '#list .player-selected') });
+
+//Assign player to a team
+const assignSelectedPlayerToTeam = (arr, teamItem, teamName, opponentTeamArr, opponentTeamName) => {
+
+    const selectedPlayer = document.querySelector('#list .player-selected');
+
+    if (!selectedPlayer) {
+        alert('Please select a player to assign');
+        return;
+    }
+
+    if (arr.length >= 5) {
+        alert(`Team: ${teamName} is full`)
+        return;
+    }
+    const playerName = selectedPlayer.textContent;
+
+    if (arr.includes(playerName)) {
+        alert(`Player: ${playerName} has already assigned to this team`)
+        return;
+    }
+
+    if (opponentTeamArr.includes(playerName)) {
+        alert(`Player ${playerName} has already assigned to team ${opponentTeamName}`)
+        return;
+    }
+
+    if (playersList.indexOf(playerName) > 9) {
+        alert('Only the first 10 players can be added to Team A or Team B')
+        return;
+    }
+    arr.push(playerName);
+    teamItem.innerHTML += `<li>${playerName}</li>`
+    selectedPlayer.classList.remove('player-selected');
+
+    console.log(arr)
+    assignEventListeners('.current-game-container li')
+}
+
+removePlayerFromTeamBtn.addEventListener("click", () => {
+    const selectedPlayerInTeamA = document.querySelector('#teamA li.player-selected');
+
+    if (selectedPlayerInTeamA) {
+        removeSelectedPlayer(arrTeamA, '#teamA li.player-selected');
+    } else {
+        removeSelectedPlayer(arrTeamB, '#teamB li.player-selected');
+    }
+});
+
+assignToTeamABtn.addEventListener('click', () => {
+    assignSelectedPlayerToTeam(arrTeamA, teamA, 'Team A', arrTeamB, 'Team B')
+});
+assignToTeamBBtn.addEventListener('click', () => {
+    assignSelectedPlayerToTeam(arrTeamB, teamB, 'Team B', arrTeamA, 'Team A')
+});
 
--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-let playersList = [];
-let playerInput = document.getElementById("player");
-let addPlayerBtn = document.getElementById("addPlayerBtn");
-let removePlayerBtn = document.getElementById("removePlayerBtn");
+export let playersList = [];
 let list = document.getElementById("list");
-let teamA = document.getElementById("teamA");
-let arrTeamA = [];
-let teamB = document.getElementById("teamB");
-let arrTeamB = [];
-let assignToTeamABtn = document.getElementById('assignToTeamABtn')
-let assignToTeamBBtn = document.getElementById('assignToTeamBBtn')
-let removePlayerFromTeamBtn = document.getElementById('removePlayerFromTeamBtn')
-
 
 //Event listeners to each list item
-const assignEventListeners = (classes) => {
+export const assignEventListeners = (classes) => {
     const listItems = document.querySelectorAll(classes);
     listItems.forEach(player => {
         player.addEventListener("click", selectPlayer);
@@ -27,50 +16,11 @@ const selectPlayer = (event) => {
     document.querySelectorAll('#list .player-selected')
         .forEach(player => player.classList.remove('player-selected'));
 
-    //document.querySelectorAll('.current-game-container li')
-      //  .forEach(player => player.classList.remove('player-selected'));
+    document.querySelectorAll('.current-game-container li')
+      .forEach(player => player.classList.remove('player-selected'));
 
     selectedPlayer.classList.toggle("player-selected");
 
-}
-
-//Add Player to list
-const addPlayer = () => {
-
-    let playerName = playerInput.value.trim();
-
-    if (playersList.includes(playerName)) {
-        alert('Player already exists');
-        return;
-    }
-
-    if (playerInput.value === '') {
-        alert('Please insert a player');
-        return;
-    }
-    playersList.push(playerInput.value.trim());
-    list.innerHTML += `<li>${playerInput.value}</li>`;
-    playerInput.value = "";
-
-    assignEventListeners('#list li');
-
-}
-
-
-//Remove player from list
-const removeSelectedPlayer = (arr, classes) => {
-    const selectedPlayer = document.querySelector(classes);
-
-    if (selectedPlayer) {
-        const index = arr.indexOf(selectedPlayer.textContent);
-        if (index !== -1) {
-            arr.splice(index, 1);
-        }
-        selectedPlayer.remove();
-    } else {
-        alert('Please select a player to remove');
-    }
-    console.log(arr)
 }
 
 //Add player automatically
@@ -85,64 +35,3 @@ const autoAddPlayers = () => {
 setInterval(autoAddPlayers, 1000);
 
 
-//Assign player to a team
-const assignSelectedPlayerToTeam = (arr, teamItem, teamName, opponentTeamArr, opponentTeamName) => {
-
-    const selectedPlayer = document.querySelector('#list .player-selected');
-
-    if (!selectedPlayer) {
-        alert('Please select a player to assign');
-        return;
-    }
-
-    if (arr.length >= 5) {
-        alert(`Team: ${teamName} is full`)
-        return;
-    }
-    const playerName = selectedPlayer.textContent;
-
-    if (arr.includes(playerName)) {
-        alert(`Player: ${playerName} has already assigned to this team`)
-        return;
-    }
-
-    if (opponentTeamArr.includes(playerName)) {
-        alert(`Player ${playerName} has already assigned to team ${opponentTeamName}`)
-        return;
-    }
-
-    if (playersList.indexOf(playerName) > 9) {
-        alert('Only the first 10 players can be added to Team A or Team B')
-        return;
-    }
-    arr.push(playerName);
-    teamItem.innerHTML += `<li>${playerName}</li>`
-    selectedPlayer.classList.remove('player-selected');
-
-    console.log(arr)
-    assignEventListeners('.current-game-container li')
-}
-
-addPlayerBtn.addEventListener("click", addPlayer);
-removePlayerBtn.addEventListener("click", () => { removeSelectedPlayer(playersList, '#list .player-selected') });
-
-removePlayerFromTeamBtn.addEventListener("click", () => {
-    const selectedPlayerInTeamA = document.querySelector('#teamA li.player-selected');
-
-    if (selectedPlayerInTeamA) {
-        removeSelectedPlayer(arrTeamA, '#teamA li.player-selected');
-    } else {
-        removeSelectedPlayer(arrTeamB, '#teamB li.player-selected');
-    }
-});
-
-assignToTeamABtn.addEventListener('click', () => {
-    assignSelectedPlayerToTeam(arrTeamA, teamA, 'Team A', arrTeamB, 'Team B')
-});
-assignToTeamBBtn.addEventListener('click', () => {
-    assignSelectedPlayerToTeam(arrTeamB, teamB, 'Team B', arrTeamA, 'Team A')
-});
-
-//Manage the list of next player
-
-export { arrTeamA, arrTeamB};
